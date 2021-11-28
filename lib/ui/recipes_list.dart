913f443b9e5ec00ef020f1ce18ae2d1eeda04e60
Maksim Nezhinskii <maksimnezhin@gmail.com>
2021-11-28 14:05:38 +0300

    Базовый интерфейс списка рецептов

---
+++ lib/ui/recipes_list.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutproj2/models/recipe_model.dart';
+class RecipesListUI extends StatelessWidget{
+  List<RecipeModel> recipes;
+
+  RecipesListUI(this.recipes, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return ListView.builder(
+      physics: ScrollPhysics(),
+      shrinkWrap: true,
+      itemCount: recipes.length,
+      itemBuilder: (BuildContext context, int index){
+          return Padding(
+            padding: const EdgeInsets.symmetric(
+                horizontal: 12,
+                vertical: 12,
+            ),
+            child: InkWell(
+              child: RecipeCard(recipes[index]),
+            ),
+          );
+        }
+      );
+  }
+}
