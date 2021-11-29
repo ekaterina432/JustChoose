76d8b690a74e8a09780d6f3eb97059639e91ed05
Maksim Nezhinskii <maksimnezhin@gmail.com>
2021-11-29 14:01:05 +0300

    Доработка страницы рецепта, добавление вкладок

--- lib/pages/recipe_details.dart
+++ lib/pages/recipe_details.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutproj2/models/recipe_model.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
-
+import 'package:tab_indicator_styler/tab_indicator_styler.dart';
 class RecipeDetails extends StatelessWidget{
   final RecipeModel recipeModel;
   RecipeDetails({required this.recipeModel});
@@ -10,7 +10,9 @@ class RecipeDetails extends StatelessWidget{
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: SlidingUpPanel(
-        borderRadius: BorderRadius.only(
+        minHeight: size.height/2,
+        maxHeight: size.height/1.2,
+        borderRadius: const BorderRadius.only(
           topLeft: Radius.circular(24),
           topRight: Radius.circular(24),
         ),
@@ -41,13 +43,13 @@ class RecipeDetails extends StatelessWidget{
                     ),
                   ),
                   Positioned(
-                    top: 20,
-                    left: 20,
+                    top: 10,
+                    left: 10,
                     child: SafeArea(
                       child:InkWell(
                         onTap: () => Navigator.pop(context),
                         child: Icon(
-                          Icons.arrow_back_ios,
+                          Icons.keyboard_arrow_left,
                           size: 32,
                           color: Theme.of(context).primaryColor,
                         ),
@@ -59,7 +61,84 @@ class RecipeDetails extends StatelessWidget{
             ],
           ),
         ),
-        panel: Text("goodnuy"),
+        panel:Padding(
+          padding: EdgeInsets.all(12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child:Container(
+                  height: 4,
+                  width: 40,
+                  decoration: BoxDecoration(
+                    color: Colors.black.withOpacity(0.3),
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30,),
+              Text(recipeModel.title, style: Theme.of(context).textTheme.headline6,),
+              SizedBox(height: 8,),
+              Row(
+                children: [
+                  Icon(Icons.star, color: Theme.of(context).primaryColor,),
+                  SizedBox(width: 4,),
+                  Text(recipeModel.rating.toString(), style: Theme.of(context).textTheme.subtitle2),
+                  SizedBox(width: 8,),
+                  Icon(Icons.schedule, color: Theme.of(context).primaryColor,),
+                  SizedBox(width: 4,),
+                  Text(recipeModel.getDurationString, style: Theme.of(context).textTheme.subtitle2),
+                  SizedBox(width: 8,),
+                  Icon(Icons.local_dining, color: Theme.of(context).primaryColor),
+                  SizedBox(width: 4,),
+                  Text("${recipeModel.servings} порц.", style: Theme.of(context).textTheme.subtitle2),
+                ],
+              ),
+              SizedBox(height: 8,),
+              Divider(color:Colors.black.withOpacity(0.3)),
+              Expanded(
+                child: DefaultTabController(
+                  length: 3,
+                  initialIndex: 0,
+                  child: Column(
+                    children: [
+                      TabBar(
+                        labelColor: Colors.black,
+                        labelStyle: TextStyle(fontSize: 12, fontWeight: FontWeight.w600),
+                        unselectedLabelColor: Colors.black.withOpacity(0.3),
+                        labelPadding: EdgeInsets.symmetric(horizontal: 0),
+                        indicator: DotIndicator (
+                          distanceFromCenter: 15,
+                        ),
+                        tabs: [
+                          Container(
+                            child: Tab(text: "Описание".toUpperCase()),
+                          ),
+                          Container(
+                            child: Tab(text: "Ингридиенты".toUpperCase()),
+                          ),
+                          Container(
+                            child: Tab(text: "Шаги".toUpperCase()),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 8,),
+                      Expanded(
+                        child: TabBarView(
+                          children: [
+                            Text("Описание"),
+                            Text("Ингридиенты"),
+                            Text("Шаги"),
+                          ],
+                        ),
+                      )
+                    ],
+                  )
+                )
+              )
+            ],
+          ),
+        )
       ),
     );
   }
