63226a0f89eff12bd43b04a11956e1409020ab20
ekkedit <ekaterinaprisepa15881@gmail.com>
2021-12-04 17:05:47 +0300

    Подключение кнопок в выборе по категориям

--- lib/categories/nothing.dart
+++ lib/categories/nothing.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutproj2/categories/baked.dart';
+import 'package:flutproj2/categories/fried_meat.dart';
+
 
 class Nothing extends StatefulWidget{
   const Nothing({Key? key}) : super(key: key);
@@ -30,9 +33,26 @@ class _NothingState extends State<Nothing>{
                         crossAxisAlignment: CrossAxisAlignment.center,
                         children:[
 
-                          const Text("Это будет реализовано немного позже :)", style: TextStyle(
-                              fontSize: 45.0,
-                              color: Colors.white54),)
+                      ElevatedButton(onPressed: (){
+                        Navigator.pushNamed( context, '/baked');
+                      },
+                          style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all(Colors.white70),
+                              textStyle: MaterialStateProperty.all(const TextStyle(fontSize: 20))),
+                          child: const  Text('запеченое', style: TextStyle(
+                              fontSize: 25,
+                              color: Colors.black54))),
+                      const Padding(padding: EdgeInsets.only(top:20),),
+                      ElevatedButton(onPressed: (){
+                        Navigator.pushNamed(context, '/friedState');
+                      },
+                          style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all(Colors.white70),
+                              textStyle: MaterialStateProperty.all( const TextStyle(fontSize: 20))),
+                          child: const Text("жаренное", style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.black54))),
+                      const Padding(padding: EdgeInsets.only(top:20),),
                         ]
                     )
                 )
--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,11 @@ import 'package:flutproj2/pages/main_screen.dart';
 import 'package:flutproj2/pages/recipes_book.dart';
 import 'package:flutproj2/pages/favorites.dart';
 import 'package:flutproj2/models/favorites_list.dart';
-import 'package:flutproj2/categories/Home_category.dart';
+import 'package:flutproj2/categories/nothing.dart';
 import 'package:flutproj2/products/home_product.dart';
+import 'package:flutproj2/categories/fried_meat.dart';
+import 'package:flutproj2/categories/baked.dart';
+
 
 void main() => runApp(
   ChangeNotifierProvider(
@@ -19,9 +22,12 @@ void main() => runApp(
         '/':(context) => MainScreen(),
         '/todo':(context) => Home(),
         '/recipes_book':(context) => RecipeBook(),
-        '/home_category': (context) => HomeCategory(),
+        '/home_category': (context) => Nothing(),
         '/home_product': (context) => HomeProduct (),
         '/favorites':(context) => FavoritesPage(),
+        '/friedState':(context) => FriedMeat(),
+        '/baked':(context) => Baked (),
+
       },
     )
   )
